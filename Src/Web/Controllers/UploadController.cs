8eb932cfb81110b0ae7c67516e62d6de6b8d01c6
Anders Juul <anders_privat@hotmail.com>
2017-07-21 21:03:30 +0200

    a

---
+++ Src/Web/Controllers/UploadController.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using WebHJ.Models;
+
+namespace WebHJ.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Post(ExpenseModel expenseModel)
+        {
+            foreach (string upload in Request.Files)
+            {
+                if (Request.Files[upload].ContentLength == 0) continue;
+                var pathToSave = Path.GetTempPath();
+                var filename = Path.GetFileName(Request.Files[upload].FileName);
+                Request.Files[upload].SaveAs(Path.Combine(pathToSave, filename));
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Src/Web/Models/ExpenseModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebHJ.Models
+{
+    public class ExpenseModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Expense Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Amount")]
+        public string Amount { get; set; }
+    }
+}
