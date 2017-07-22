9861b922aac01c65ff90a0ffd9bb61b83adb4775
Anders Juul <anders_privat@hotmail.com>
2017-07-22 08:40:18 +0200

    a

--- Src/MessageHandlers/Worker.cs
+++ Src/MessageHandlers/Worker.cs
@@ -29,13 +29,14 @@ namespace HansJuergenWeb.MessageHandlers
         {
             try
             {
-                _backgroundWorker = new BackgroundWorker
-                {
-                    WorkerSupportsCancellation = true
-                };
-                _backgroundWorker.DoWork += _backgroundWorker_DoWork;
-                _backgroundWorker.RunWorkerCompleted += _backgroundWorker_RunWorkerCompleted;
-                _backgroundWorker.RunWorkerAsync();
+                SetupSubscriptions();
+                //_backgroundWorker = new BackgroundWorker
+                //{
+                //    WorkerSupportsCancellation = true
+                //};
+                //_backgroundWorker.DoWork += _backgroundWorker_DoWork;
+                //_backgroundWorker.RunWorkerCompleted += _backgroundWorker_RunWorkerCompleted;
+                //_backgroundWorker.RunWorkerAsync();
             }
             catch (Exception ex)
             {
@@ -44,26 +45,25 @@ namespace HansJuergenWeb.MessageHandlers
             }
         }
 
-        private void _backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            WorkDone = true;
-        }
+        //private void _backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        //{
+        //    WorkDone = true;
+        //}
 
-        private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            Log.Logger.Information("Doing work");
+        //private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        //{
+        //    Log.Logger.Information("Doing work");
 
-            try
-            {
-                SetupSubscriptions();
-                DoWorkInternal(sender);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "During do work", new object[0]);
-                throw;
-            }
-        }
+        //    try
+        //    {
+        //        DoWorkInternal(sender);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.Error(ex, "During do work", new object[0]);
+        //        throw;
+        //    }
+        //}
 
         private void SetupSubscriptions()
         {
@@ -88,137 +88,38 @@ namespace HansJuergenWeb.MessageHandlers
                 }));
         }
 
-        //private void SetupRabbit()
+        //private void DoWorkInternal(object sender)
         //{
-        //    var connectioFa = new ConnectionFactory
+        //    WorkDone = false;
+
+        //    var lastSend = DateTime.MinValue;
+
+        //    while (true)
         //    {
-        //        HostName = "ajf-elastic-01",
-        //        UserName = "anders",
-        //        Password = "21Bananer"
-        //    };
+        //        var backgroundWorker = sender as BackgroundWorker;
+        //        if (backgroundWorker == null || backgroundWorker.CancellationPending)
+        //        {
+        //            Log.Information("backgroundworker.CancellationPending: {@backgroundWorker}", backgroundWorker);
+        //            return;
+        //        }
 
-        //    var connection = connectioFa.CreateConnection();
-        //    _model = connection.CreateModel();
-
-        //    _model.QueueDeclare(_appSettings.QueueName, true, false, false);
-
-        //    _model.ExchangeDeclare(_appSettings.ExchangeName, ExchangeType.Topic);
-
-        //    _model.QueueBind(_appSettings.QueueName, _appSettings.ExchangeName, "");
-
-        //    Log.Logger.Information("Done setting up queue and exchange...");
-
+        //        Thread.Sleep(1 * 1000);
+        //    }
         //}
 
-        private void DoWorkInternal(object sender)
-        {
-            WorkDone = false;
-
-            var lastSend = DateTime.MinValue;
-
-            while (true)
-            {
-                var backgroundWorker = sender as BackgroundWorker;
-                if (backgroundWorker == null || backgroundWorker.CancellationPending)
-                {
-                    Log.Information("backgroundworker.CancellationPending: {@backgroundWorker}", backgroundWorker);
-                    return;
-                }
-
-                Thread.Sleep(1 * 1000);
-                //bool noAck = false;
-                //BasicGetResult result = _model.BasicGet(_appSettings.QueueName, noAck);
-                //if (result == null)
-                //{
-                //    // No message available at this time.
-                //}
-                //else
-                //{
-                //    IBasicProperties props = result.BasicProperties;
-                //    byte[] body = result.Body;
-                //    _model.BasicAck(result.DeliveryTag,);
-                //}
-                //if (_appSettings.Minutes.Contains(DateTime.Now.Minute))
-                //{
-                //    if (_appSettings.Hours.Contains(DateTime.Now.Hour))
-                //    {
-                //        if (DateTime.Now.Subtract(lastSend) > _appSettings.MinSleep)
-                //        {
-                //            Log.Information("AppSettings match up with current time. Sending.");
-                //            foreach (var job in _appSettings.Jobs)
-                //            {
-                //                SendMail(job.Path, job.Receiver).Wait();
-                //            }
-                //            lastSend = DateTime.Now;
-                //        }
-                //    }
-                //}
-            }
-        }
-
-        //private async Task SendMail(string path, string email)
-        //{
-        //    var folderContents = GetFolderContents(path, path).ToArray();
-
-
-        //    var fromMailAddress = new MailAddress("andersjuulsfirma@gmail.com");
-        //    var networkCredential = new NetworkCredential("azure_9ce305d9c6ab84b3d5fb7723deca51f5@azure.com",
-        //        "6oi3vEdJl5GldVc");
-        //    var transport = new Web(networkCredential);
-
-
-        //    var subjectLine = "Filer paa JuulNas, " + path;
-
-        //    // Create the email object first, then add the properties.
-        //    var myMessage = new SendGridMessage { From = fromMailAddress };
-
-        //    myMessage.AddTo(email);
-        //    myMessage.AddCc("andersjuulsfirma@gmail.com");
-
-        //    var bytes = Encoding.Default.GetBytes(subjectLine);
-        //    myMessage.Subject = Encoding.UTF8.GetString(bytes);
-
-        //    var html = $"Filer i {path}:<BR/>";
-        //    html += "--<BR/>";
-        //    html += $"Ialt : {folderContents.Count()}<BR/>";
-        //    html += "--<BR/>";
-        //    foreach (var line in folderContents)
-        //    {
-        //        html += line + "<BR/>";
-        //    }
-        //    myMessage.Html = html;
-
-        //    // Send the email.
-        //    await transport.DeliverAsync(myMessage);
-        //}
-
-        //private IEnumerable<string> GetFolderContents(string path, string originalPath)
-        //{
-        //    var result = new List<string>();
-        //    foreach (var file in Directory.GetFiles(path).OrderByDescending(x => x))
-        //    {
-        //        result.Add(file.Replace(originalPath, ""));
-        //    }
-
-        //    foreach (var dir in Directory.GetDirectories(path).OrderByDescending(x => x))
-        //    {
-        //        result.AddRange(GetFolderContents(dir, originalPath));
-        //    }
-        //    return result;
-        //}
 
 
         public void Stop()
         {
-            if (_backgroundWorker != null)
-            {
-                _backgroundWorker.CancelAsync();
+            //if (_backgroundWorker != null)
+            //{
+            //    _backgroundWorker.CancelAsync();
 
-                while (!WorkDone)
-                {
-                    Thread.Sleep(500);
-                }
-            }
+            //    while (!WorkDone)
+            //    {
+            //        Thread.Sleep(500);
+            //    }
+            //}
         }
     }
 }
