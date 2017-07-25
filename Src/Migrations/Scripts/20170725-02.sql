667df81ae6fde5e29bae80aceb7a3f42a335128b
Anders Juul <anders_privat@hotmail.com>
2017-07-25 13:37:29 +0200

    a

---
+++ Src/Migrations/Scripts/20170725-02.sql
@@ -0,0 +1,6 @@
+﻿DROP TABLE [dbo].[Foo];
+
+CREATE TABLE [dbo].[Allergenes]([Id] int IDENTITY(1,1), [Name] NVARCHAR(MAX) NOT NULL )
+CREATE TABLE [dbo].[Uploaders]([Id] int IDENTITY(1,1), [Name] NVARCHAR(MAX) NOT NULL, [Email] NVARCHAR(MAX) NOT NULL)
+CREATE TABLE [dbo].[FCResults]([Id] int IDENTITY(1,1), [Uploader_Id] int NOT NULL, [Allergene_Id] int NOT NULL, [UploadGuid] NVARCHAR(MAX) NOT NULL)
+CREATE TABLE [dbo].[AllergeneSubscriptions]([Id] int IDENTITY(1,1), [Allergene_Id] int NOT NULL, [Uploader_Id] int NOT NULL)
