1b60e23bbf7adb68287bea26c812ba53e70e6259
Anders Juul <anders_privat@hotmail.com>
2017-07-25 13:08:38 +0200

    a

--- Src/Migrations/Program.cs
+++ Src/Migrations/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using DbUp;
 
 namespace Migrations
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var connectionString = ConfigurationManager.AppSettings["FlowCytoConnection"];
+
+            var upgradeEngine = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+            Debug.WriteLine(upgradeEngine);
+
+            var result = upgradeEngine.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
